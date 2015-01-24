c92cc406e732b77d1ffb685bc3ea8978f918a78f
David Deschamps <david@v1020-wn-75-154.campus-dynamic.uwaterloo.ca>
2015-01-23 21:07:03 -0500

    basic players with rope. one player is fixed, other moves left/right with arrows

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigidbody.AddForce (new Vector3 (Input.GetAxis("Horizontal")*10, 0));
+		
+		transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+		transform.position = new Vector3(transform.position.x,transform.position.y,0);
+	}
+}
