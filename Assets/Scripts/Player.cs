150268ab38a9b9e8f31fae1a7af6c54b97761a0e
David Deschamps <david@v1020-wn-75-154.campus-dynamic.uwaterloo.ca>
2015-01-24 16:52:09 -0500

    shaders and patterns and levels, oh my

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,11 +26,19 @@ public class Player : MonoBehaviour {
 	public KeyCode right = KeyCode.RightArrow;
 	public KeyCode grab = KeyCode.RightShift;
 
+	private bool onHandle = false;
+
+	public bool isAtGoal = false;
 
 	private float spawnPointTime;
 	private Vector3 spawnPoint;
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (Input.GetKey (grab) && onHandle) {
+			rigidbody.isKinematic = true;
+		} else {
+			rigidbody.isKinematic = false;
+		}
 		if (Input.GetKey (jump) && IsGrounded() ) {
 			rigidbody.AddForce(new Vector3(0,jumpSpeed,0));
 		}
@@ -40,20 +48,30 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey (right) && rigidbody.velocity.x < maxSpeed) {
 			rigidbody.AddForce(new Vector3(moveSpeed,0,0));
 		}
-		if (Input.GetKeyDown(grab)) {
-			rigidbody.isKinematic = true;
-		}
-		if (Input.GetKeyUp (grab)) {
-			rigidbody.isKinematic = false;
-		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Death")) {
-			Respawn();		
+			Respawn ();		
 		} else if (other.CompareTag ("CheckPoint")) {
 			spawnPoint = other.transform.position;
 			spawnPointTime = Time.time;
+		} else if (other.CompareTag ("Goal")) {
+			isAtGoal = true;
+			if (partner.isAtGoal) {
+					int i = Application.loadedLevel;
+					Application.LoadLevel (i + 1);
+			}
+		} else if (other.CompareTag ("Handle")) {
+			onHandle = true;		
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Goal")) {
+			isAtGoal = false;
+		} else if (other.CompareTag ("Handle")) {
+			onHandle = false;
 		}
 	}
 
