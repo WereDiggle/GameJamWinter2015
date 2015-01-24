aacce1c18d793967eaa82e5028a1b7f2f0e7fd36
David Deschamps <david@v1020-wn-75-154.campus-dynamic.uwaterloo.ca>
2015-01-23 21:35:07 -0500

    better player controls, TestLLevel has a bouncy trampoline

---
+++ Assets/Fixed2D.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fixed2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+		transform.position = new Vector3(transform.position.x,transform.position.y,0);
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,13 +7,30 @@ public class Player : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	public float jumpSpeed = 5;
+	public float moveSpeed = 5;
+
+	public KeyCode jump = KeyCode.UpArrow;
+	public KeyCode left = KeyCode.LeftArrow;
+	public KeyCode right = KeyCode.RightArrow;
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (jump) && IsGrounded ()) {
+			rigidbody.AddForce(new Vector3(0,jumpSpeed,0));
+		}
+		if (Input.GetKeyDown (left)) {
+			rigidbody.AddForce(new Vector3(-moveSpeed,0,0));
+		}
+		if (Input.GetKeyDown (right)) {
+			rigidbody.AddForce(new Vector3(moveSpeed,0,0));
+		}
 
 		rigidbody.AddForce (new Vector3 (Input.GetAxis("Horizontal")*10, 0));
-		
-		transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
-		transform.position = new Vector3(transform.position.x,transform.position.y,0);
+	}
+
+	bool IsGrounded() {
+		return Physics.CheckCapsule(collider.bounds.center,new Vector3(collider.bounds.center.x,collider.bounds.min.y-0.1f,collider.bounds.center.z),0.18f);
 	}
 }
