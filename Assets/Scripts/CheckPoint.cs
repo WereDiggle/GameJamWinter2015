5f4b75e4650d97994b28d8b1d060e4f66e98c04e
WereDiggle <archdiggle@gmail.com>
2015-01-25 13:06:03 -0500

    checkpoint stuff

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,9 +6,13 @@ public class CheckPoint : MonoBehaviour {
 
 	public List<GameObject> objectsToReset;
 
+	private List<Vector3> locations;
+
 	// Use this for initialization
 	void Start () {
-	
+		foreach (GameObject resetObject in objectsToReset) {
+			locations.Add(resetObject.transform.position);
+		}
 	}
 	
 	// Update is called once per frame
@@ -18,9 +22,18 @@ public class CheckPoint : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
 		if (other.CompareTag ("Player")) {
+			//sets player spawn points
 			Player p = other.gameObject.GetComponent<Player>();
 			p.spawnPoint = p.transform.position;
 			p.spawnPointTime = Time.timeSinceLevelLoad;
+
+			//puts objects back in place
+			int i=0;
+			foreach (GameObject resetObject in objectsToReset) {
+				resetObject.transform.position = locations[i];
+				i++;
+			}
+
 		}
 	}
 }
