ea539e561f267e1e0077367cd8da260273cc71c9
David Deschamps <david@v1020-wn-75-154.campus-dynamic.uwaterloo.ca>
2015-01-25 13:05:22 -0500

    level two puzzle, fixed2d with z-rotation

---
+++ Assets/Scripts/Fixed2DAllowRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fixed2DAllowRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { 
+		if (gameObject.GetComponent("Rigidbody") != null)
+			rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+		transform.position = new Vector3(transform.position.x,transform.position.y,0);*/
+	}
+}
